b9f7c9a001bf7dee76531af7d99c64cd873d52a5
김재부 <63542597+depensive21@users.noreply.github.com>
2026-05-20 17:37:31 +0900

    DB 설정파일 , 파이선서버 설정파일 업데이트 및 업로드

---
+++ Proxy/config/database.js
@@ -0,0 +1,30 @@
+// DB와 연결되어진 통로정보를 가지고있는 파일입니다.
+
+// mysql 설치 : npm install mysql2
+
+
+// 01. mysql2 모듈 가져오기
+const mysql = require("mysql2");
+
+
+
+// 02. 사용할 DB의 정보를 정의
+const db_info = {  
+    host : "project-db-campus.smhrd.com",
+    user : "cd_25K_HI5_p2_4",
+    password : "smhrd4",
+    port : "3312",
+    database : "cd_25K_HI5_p2_4"
+}
+
+// 03. DB 연결할수있는 객체 생성
+
+module.exports = mysql.createConnection( db_info );
+
+
+// 김재부
+// URL : project-db-campus.smhrd.com
+// PORT : 3312
+// USER : cd_25K_HI5_p2_4
+// PW : smhrd4
+// DataBase : cd_25K_HI5_p2_4
---
+++ Proxy/config/pythonFastAPI.js
@@ -0,0 +1,17 @@
+// Python 와 연결되어진 통로정보를 가지고있는 파일입니다.
+
+
+// 📄 path: src/config/pythonFastAPI.js
+
+const PYTHON_SERVER_BASE = "http://localhost:8000";
+
+const pythonConfig = {
+    // 1. 기본 주소
+    baseUrl: PYTHON_SERVER_BASE,
+    
+    // 2. 기능별 최종 전체 주소 목록
+    predictAllUrl: `${PYTHON_SERVER_BASE}/predict-all`
+};
+
+// 밖에서 require()로 가져다 쓸 수 있도록 내보내기
+module.exports = pythonConfig;
