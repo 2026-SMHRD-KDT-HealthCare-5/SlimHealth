be2538016ed536569ab5b7e22faac9f396fba5ba
JJU <462123@naver.com>
2026-05-26 10:55:24 +0900

    FastAPI.py cors미들웨어 추가

--- Python/Server/FastAPI.py
+++ Python/Server/FastAPI.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib, uvicorn
 import pandas as pd
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models  = joblib.load("health_models_v4.pkl")
 results = joblib.load("health_model_scores_v4.pkl")
