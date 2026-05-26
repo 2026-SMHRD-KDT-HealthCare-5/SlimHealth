529442d494b073466a2efc4670ba131c46859c93
김재부 <63542597+depensive21@users.noreply.github.com>
2026-05-26 11:29:05 +0900

    OCR 기능 main연결, 파이선전송예시 등

--- Proxy/main.js
+++ Proxy/main.js
@@ -1,30 +1,27 @@
 
 
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 const cors = require("cors");
 
 app.use(cors({
-    origin: "http://localhost:5173", // 💡 리액트(Axios)가 출발하는 주소를 정확히 명시!
-    credentials: true                
+    origin: "http://localhost:5173",
+    credentials: true
 }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
-app.use( express.json() );
+const healthRouter = require("./Router/healthRouter");
+const userRouter = require("./Router/userRouter");
+const ocrRouter = require("./Router/ocrRouter");
 
-
-const healthRouter = require("./Router/healthRouter")
-const userRouter   = require("./Router/userRouter")
-
-
-app.use("/api/health",  healthRouter);
+app.use("/api/health", healthRouter);
 app.use("/api/user", userRouter);
+app.use("/api/ocr", ocrRouter);
 
-
-
-app.listen(8000, ()=>{
-    console.log("node 중계서버, 8000 포트에서 사용중")
-})
+app.listen(8000, () => {
+    console.log("node 중계서버, 8000 포트에서 사용중");
+});
