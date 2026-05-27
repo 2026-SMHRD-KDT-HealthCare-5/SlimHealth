c223ab09234fd3110e7df4e22d56c6178ffcb605
김재부 <63542597+depensive21@users.noreply.github.com>
2026-05-27 11:12:54 +0900

    main.js  JWT 토큰 적용

--- Proxy/main.js
+++ Proxy/main.js
@@ -14,12 +14,14 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+const authRequired = require("./middleware/authRequired");
 const healthRouter = require("./Router/healthRouter");
 const userRouter = require("./Router/userRouter");
 const ocrRouter = require("./Router/ocrRouter");
 
-app.use("/api/health", healthRouter);
 app.use("/api/user", userRouter);
+app.use("/api/health", authRequired, healthRouter);
 app.use("/api/ocr", ocrRouter);
 
 app.listen(8000, () => {
